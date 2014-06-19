af2377b9b91d1fef01e642ef2e33afa18588573a
Joel Bernstein <bergamot@gmail.com>
2014-06-19 11:19:59 -0700

    Initial Commit

---
+++ JBAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBAnimation
+//
+//  Created by Joel Bernstein on 6/19/14.
+//  Copyright (c) 2014 Joel Bernstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JBAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JBAnimation
+//
+//  Created by Joel Bernstein on 6/19/14.
+//  Copyright (c) 2014 Joel Bernstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
